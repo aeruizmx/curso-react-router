d3bde6718580f0b593112d7fb633a891fae434ad
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-01 10:16:47 -0600

    Finalizando reducers y eliminar favoritos

--- PlatziVideo/src/reducers/index.js
+++ PlatziVideo/src/reducers/index.js
@@ -1,5 +1,26 @@
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case 'SET_FAVORITE': {
+      const found = state.myList.filter((item) => item.id === action.payload.id);
+      if (found.length === 0) {
+        return {
+          ...state,
+          myList: [...state.myList, action.payload],
+        };
+      }
+      return state;
+    }
+    break;
+    case 'DELETE_FAVORITE': {
+      return {
+        ...state,
+        myList: state.myList.filter((item) => item.id !== action.payload),
+      };
+    }
+    break;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
