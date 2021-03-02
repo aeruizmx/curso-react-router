76ba8d6ecda199fbe7f09410d496fe2730325a5c
Andres Ruiz <aruiz@grupooctano.com.mx>
2021-03-02 12:16:55 -0600

    Vista general del player

--- PlatziVideo/src/components/CarouselItem.jsx
+++ PlatziVideo/src/components/CarouselItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { setFavorite, deleteFavorite } from '../actions/index';
 import '../assets/styles/components/CarouselItem.scss';
@@ -22,7 +23,9 @@ const CarouselItem = (props) => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img className='carousel-item__details--img' src={playIcon} alt='Play icon' />
+          <Link to={`/player/${id}`}>
+            <img className='carousel-item__details--img' src={playIcon} alt='Play icon' />
+          </Link>
           {isList ?
             <img className='carousel-item__details--img' src={removeIcon} alt='Plus icon' onClick={() => handleDeleteFavorite(id)} /> :
             <img className='carousel-item__details--img' src={plusIcon} alt='Plus icon' onClick={handleSetFavorite} />}
